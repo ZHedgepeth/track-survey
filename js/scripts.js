0da35ab52fba874b13807b2fdad03ec21cf6b5cc
Zach Hedgepeth <Zhedgepeth1124@gmail.com>
2016-08-15 00:20:33 -0400

    updating project, still needs work

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,104 @@ $(document).ready(function() {
 
   $("form#info").submit(function(event) {
 
-  event.preventDefault();
+    event.preventDefault();
 
-  //collect variables
+var experience = $("input:radio[name=experience]:checked").val();
+var industry = $("input:radio[name=industry]:checked").val();
+var interaction = $("input:radio[name=interaction]:checked").val();
+var companyType = $("input:radio[name=companyType]:checked").val();
+var application = $("input:radio[name=application]:checked").val();
+// var company = $("input:radio[name=company]:checked").val();
 
-  var experience = parseInt($("input:radio[name=experience]:checked").val());
+  if (experience === 'a-lot') {
 
-  if (experience >= 7) {
-      $("#php").show();
-  }
-});
+    if (industry === 'web-dev') {
+
+      if (interaction === 'heavy') {
+
+        if (companyType === 'enterprise') {
+
+        if (application === 'android') {
+          $('#java').show();
+        }  else if (application === 'web') {
+          $('#ruby').show();
+        } else {
+          $('#csharp').show();
+        }
+      }
+    
+
+
+//
+//     if (experience === 'none')
+//     if (industry === 'web-dev')
+//     if (interaction === 'little')
+//     if (companyType === 'startup')
+//     { $('#php').show(); }
+//      if (application === 'web')
+//       { $('#php').show(); }
+//      else if (application === 'windows')
+//       { $('#csharp').show();}
+//
+//
+// });
+
+
+
+
+
+
+
+
+// })
+//     if (experience === 'none')
+//     if (industry === 'software')
+//     if (interaction === 'heavy' || interaction === 'little')
+//     if (companyType === 'startup')
+//     if (application === 'web') {
+//       $('#php').show();
+// }
+
+
+//    if (industry === 'software') {
+//
+//     if (interaction === 'heavy') {
+//
+//       if (company-type === 'large') {
+//
+//         if (company === 'stack')
+//           //  $("#csharp").show();
+//         alert("csharp");
+// }
+// }
+// }
+// }
+//     } else {
+//
+//       if (experience === 'a-lot') {
+//
+//       if (industry === 'software') {
+//
+//         if (interaction === 'heavy') {
+//
+//           if (company-type === 'enterprise') {
+//
+//             if (company === 'facebook')
+//             $('$php').show();
+//           }
+//         }
+//       }
+//
+// }
+// }
+// };
+// };
+  // } else if (company-type === "startup") {
+  //   $("#ruby").show();
+  //
+  // } else if (company-type === "large") {
+  //   $("#php").show();
+  //
+  // } else  {
+  //   $("#java").show();
+  // }
